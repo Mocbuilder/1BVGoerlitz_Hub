25f12cf6298f455a202beb737f0f57abf4ea9c21
Mocbuilder Coding Creations <moc.build.channel@gmail.com>
2023-12-16 18:25:08 +0100

    added more content + bug/style fixes

--- 1BVGoerlitz_Hub/Pages/Kalender.cshtml.cs
+++ 1BVGoerlitz_Hub/Pages/Kalender.cshtml.cs
@@ -1,12 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _1BVGoerlitz_Hub.Pages
 {
     public class KalenderModel : PageModel
     {
+        public List<string> PdfPaths { get; set; }
+
         public void OnGet()
         {
+
+            var pdfFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "EventPDF");
+
+
+            PdfPaths = Directory.GetFiles(pdfFolderPath, "*.pdf")
+                .Select(filePath => GetRelativePath(filePath))
+                .ToList();
+        }
+
+        // Helper method to get the relative path of a file
+        private string GetRelativePath(string filePath)
+        {
+            var wwwRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            return filePath.Replace(wwwRootPath, "").Replace("\\", "/");
         }
     }
 }
